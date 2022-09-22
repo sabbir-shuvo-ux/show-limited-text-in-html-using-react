ec639b14faf6ec5aa9bd588e60052d9d6a3364a5
sabbir-shuvo-ux <shuvosabbir134@gmail.com>
2022-09-22 12:42:44 +0600

    show limited text in html using js

---
+++ index.js
@@ -0,0 +1,9 @@
+ const overviewElips = (str, num)=>{
+            if(str?.length > num){
+                  return str.slice(0, num) + "...";
+            }else{
+                  return str;
+            }
+      }
+      
+overviewElips(textHere, how many chartcter you wanna show num here)
